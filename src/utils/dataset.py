f20de9cc6b22d2ec70035c49cd052c2ff0e6519c
Yasmin Farisah Salma <114626408+caernations@users.noreply.github.com>
2025-03-27 11:58:29 +0700

    change dataset split

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -2,18 +2,23 @@ from sklearn.datasets import fetch_openml
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-def load_mnist(test_split=0.2, normalize=True, one_hot=True):
-    print("downloading")
+def load_mnist(normalize=True, one_hot=True):
+    print("Downloading MNIST...")
     mnist = fetch_openml('mnist_784', version=1, parser='pandas', as_frame=False)
-    X, y = mnist.data, mnist.target.astype(int)  
-    print(f"loaded successfully! Shape: {X.shape}, Labels: {y.shape}")
+    X, y = mnist.data, mnist.target.astype(int)
+    
+    # Split original MNIST (60k train, 10k test)
+    X_train, X_test = X[:60000], X[60000:]
+    y_train, y_test = y[:60000], y[60000:]
+    
+    print(f"Original split - Train: {len(X_train)}, Test: {len(X_test)}")
 
     if normalize:
-        X = X / 255.0
+        X_train = X_train / 255.0
+        X_test = X_test / 255.0
+        
     if one_hot:
-        y = np.eye(10)[y] 
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_split, random_state=42)
-    print(f"dataset displit menjadi {len(X_train)} training data sdan {len(X_test)} testing data.")
+        y_train = np.eye(10)[y_train]
+        y_test = np.eye(10)[y_test]
 
     return X_train, X_test, y_train, y_test
